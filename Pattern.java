4659fca59e0ef743cf89edbe38fce9f18b7f0701
yash2316 <141305637+yash2316@users.noreply.github.com>
2024-07-31 19:28:13 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -5,6 +5,6 @@ public class Pattern {
     public static void main(String args[])
     {
          System.out.println("Hello world");
-        System.out.println("Test 3 ");
+        System.out.println("Test 4 ");
     }
 }
